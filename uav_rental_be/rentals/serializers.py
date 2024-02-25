7e3933f29492e0cb199bc6ba2a66fec561eade2e
furknozg <fozgultekin19@ku.edu.tr>
2024-02-26 02:02:01 +0300

    Patch: Added additional serializer for update

--- uav_rental_be/rentals/serializers.py
+++ uav_rental_be/rentals/serializers.py
@@ -2,6 +2,20 @@ from rest_framework import serializers
 from .models import Rental
 
 class RentalSerializer(serializers.ModelSerializer):
+    uav_name = serializers.CharField(source='uav.brand', read_only=True)
+    uav_owner = serializers.CharField(source='uav.owner.username', read_only=True)
+    uav = serializers.CharField(source='uav.id', read_only=True)
+    renter = serializers.CharField(source='rental.renter', read_only=True)
+
+
     class Meta:
         model = Rental
-        fields = ['id', 'uav', 'renter', 'start_date', 'end_date']
+        fields = ['id', 'uav', 'renter', 'uav_name', 'uav_owner', 'start_date', 'end_date']
+
+class RentalCreateSerializer(serializers.ModelSerializer):
+    uav_name = serializers.CharField(source='uav.brand', read_only=True)
+    uav_owner = serializers.CharField(source='uav.owner.username', read_only=True)
+    renter = serializers.CharField(source='rental.renter', read_only=True)
+    class Meta:
+        model = Rental
+        fields = ['id', 'uav', 'renter', 'uav_name', 'uav_owner', 'start_date', 'end_date']
